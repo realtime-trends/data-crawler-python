74dc7999694b78b45eff7c1f907c126e3a925f33
Changho Park <hoya.develop@gmail.com>
2022-03-27 14:54:28 +0900

    feat: Update updater codes.

---
+++ main.py
@@ -0,0 +1,109 @@
+import json
+from datetime import datetime, timezone
+from typing import Any, Dict, List, Union
+
+from src import Trend, calculate_trends
+from src.crawl import set_delta
+
+TREND_FILE = "trends.json"
+INTERVAL_SECS = 600
+
+
+class TrendJson:
+    @classmethod
+    def read(cls) -> Dict:
+        with open(TREND_FILE, "r") as trend_json:
+            cls.json_dict = json.load(trend_json)
+
+    @classmethod
+    def write(cls):
+        with open(TREND_FILE, "w") as trend_json:
+            json.dump(cls.json_dict, trend_json, ensure_ascii=False, indent=4)
+
+    @classmethod
+    def update(cls, key: str, value: Union[List, Dict], json_dict: Any = None) -> Dict:
+        if json_dict == None:
+            json_dict = cls.json_dict
+        keys = key.strip("/").split("/")
+        selected_key = keys[0]
+        if len(keys) == 1:
+            json_dict[selected_key] = value
+        elif len(keys) >= 2:
+            selected_dict = json_dict.get(selected_key, {})
+            json_dict[selected_key] = TrendJson.update(
+                "/".join(keys[1:]), value, selected_dict
+            )
+
+        return json_dict
+
+    @classmethod
+    def get(cls, key: str, default: Any, json_dict: Any = None) -> Dict:
+        if json_dict == None:
+            json_dict = cls.json_dict
+        if key == None:
+            return json_dict
+
+        keys = key.strip("/").split("/")
+        selected_key = keys[0]
+        if selected_key not in json_dict.keys():
+            return default
+
+        json_dict = json_dict[selected_key]
+        if len(keys) >= 2:
+            return TrendJson.get("/".join(keys[1:]), default, json_dict)
+        return json_dict
+
+    @classmethod
+    def delete(cls, key: str, json_dict: Any = None) -> Dict:
+        if json_dict == None:
+            json_dict = cls.json_dict
+        if key == None:
+            return None
+
+        keys = key.strip("/").split("/")
+        selected_key = keys[0]
+        if selected_key not in json_dict.keys():
+            return
+
+        if len(keys) == 1:
+            del json_dict[selected_key]
+        elif len(keys) >= 2:
+            json_dict[selected_key] = TrendJson.delete(
+                "/".join(keys[1:]), json_dict[selected_key]
+            )
+            if TrendJson.get(f"{selected_key}/{keys[1]}", None, json_dict) == {}:
+                json_dict = TrendJson.delete(f"{selected_key}/{keys[1]}", json_dict)
+        return json_dict
+
+
+if __name__ == "__main__":
+    TrendJson.read()
+
+    now = int(datetime.now(timezone.utc).timestamp())
+    new_timestamp = str(now)
+    new_trends = calculate_trends()
+
+    timestamps: List[int] = TrendJson.get("timestamps", [])
+    old_timestamps = []
+    if timestamps:
+        old_timestamps = sorted(
+            [timestamp for timestamp in timestamps if timestamp <= now - INTERVAL_SECS],
+            reverse=True,
+        )
+    old_trends = []
+    if old_timestamps:
+        old_trends = [Trend(**trend) for trend in TrendJson.get(old_timestamps[0])]
+
+    new_trends = set_delta(new_trends, old_trends)
+    new_trends_dict = [vars(trend) for trend in new_trends]
+    print(new_trends_dict)
+    TrendJson.update(new_timestamp, new_trends_dict)
+
+    if len(old_timestamps) >= 2:
+        for old_timestamp in old_timestamps[1:]:
+            TrendJson.delete(old_timestamp)
+        timestamps = list(set(timestamps) - set(old_timestamps[1:]))
+    timestamps.append(new_timestamp)
+    TrendJson.update("timestamps", timestamps)
+
+    TrendJson.write()
---
+++ src/crawl.py
@@ -0,0 +1,84 @@
+import warnings
+from typing import Dict, List
+
+import requests
+from bs4 import BeautifulSoup
+
+from . import Trend
+
+WEIGHTS = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
+ENGINE_BIAS = {
+    "nate": 0.7,
+    "zum": 1.0,
+}
+SIMILARITY_WEIGHT = 0.7
+
+
+def get_trends_by_engine(engine: str) -> List[Trend]:
+    trends: List[Trend] = []
+    keywords: List[str] = []
+
+    if engine == "zum":
+        url = "https://search.zum.com/search.zum"
+        req = requests.get(url)
+        if req.status_code == 200:
+            html = req.text
+            soup = BeautifulSoup(html, "lxml", from_encoding="utf-8")
+            keywords = [
+                s.text
+                for s in soup.select(
+                    "#issue_wrap > ul > li > div > a:nth-child(1) > span.txt"
+                )
+            ]
+    elif engine == "nate":
+        url = "https://www.nate.com/js/data/jsonLiveKeywordDataV1.js"
+        req = requests.get(url)
+        if req.status_code == 200:
+            keywords = [j[1] for j in req.json()]
+    for index, keyword in enumerate(keywords):
+        score = WEIGHTS[index] * ENGINE_BIAS[engine]
+        trends.append(Trend(keyword, score, score))
+    return trends
+
+
+def set_delta(new_trends: List[Trend], old_trends: List[Trend]):
+    for new_index, new_trend in enumerate(new_trends):
+        for old_index, old_trend in enumerate(old_trends):
+            if new_trend.hashed == old_trend.hashed:
+                new_trend.delta = old_index - new_index
+        new_trends[new_index] = new_trend
+    return new_trends
+
+
+def calculate_trends() -> List[Trend]:
+    trends: Dict[str, Trend] = {}
+    for engine in ENGINE_BIAS.keys():
+        for trend in get_trends_by_engine(engine):
+            hashed = trend.hashed
+            if hashed in trends.keys():
+                t_trend = trends[hashed]
+                t_trend.add(trend.score, trend.maxscore)
+            else:
+                t_trend = trend
+            trends[hashed] = t_trend
+
+    for trend1 in list(trends.values()):
+        hashed1 = trend1.hashed
+        is_contain = False
+        for trend2 in trends.values():
+            hashed2 = trend2.hashed
+            if hashed1 == hashed2:
+                continue
+            if trend2.keyword in trend1.keyword:
+                trend2.add(trend1.score * SIMILARITY_WEIGHT, trend1.maxscore)
+                trends[hashed2] = trend2
+                is_contain = True
+        if is_contain:
+            del trends[hashed1]
+    return sorted(trends.values(), key=lambda x: x.score, reverse=True)
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings(action="ignore")
+
+    print(calculate_trends())
